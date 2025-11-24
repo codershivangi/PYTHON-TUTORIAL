4e8ccdfc60654190ea872cb7e11b59e3255e69ed
Shivangi <159949628+codershivangi@users.noreply.github.com>
2025-11-23 20:43:40 -0800

    Add files via upload

---
+++ Chapter 3/str_function.py
@@ -0,0 +1,9 @@
+name = "harry"
+print(len(name))
+print(name.endswith("rry"))
+print(name.startswith("ha"))
+print(name.capitalize())
+
+a = "harry is a good good boy"
+b = a.replace("good","bad")
+print(b)
